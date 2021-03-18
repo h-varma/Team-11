1171071fbcee949ad1af486b5f0d14b976355bee
Jonas Leitner <j.leitner@stud.uni-heidelberg.de>
2021-03-18 14:57:42 +0100

    doc test2

--- doc/build/html/_sources/index.rst.txt
+++ doc/build/html/_sources/index.rst.txt
@@ -1,5 +1,5 @@
 .. analysis documentation master file, created by
-   sphinx-quickstart on Thu Mar 18 14:38:44 2021.
+   sphinx-quickstart on Thu Mar 18 14:56:43 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../analysis/'))
+sys.path.insert(0, os.path.abspath('../sry/'))
 
 
 # -- Project information -----------------------------------------------------
@@ -22,7 +22,7 @@ copyright = '2021, Johannes, Hridya, Jonas'
 author = 'Johannes, Hridya, Jonas'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,7 +30,8 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
