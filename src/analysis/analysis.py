ec21948497347b5613edcd892bffb87d9126c6b2
Johannes Fuerle <johannes.fuerle@uni-heidelberg.de>
2021-03-18 13:43:24 +0100

    added functions in statistical method, not yet finished

--- src/analysis/analysis.py
+++ src/analysis/analysis.py
@@ -24,7 +24,31 @@ class data_preparation:
     pass
 
 class statistical_methods:
-    pass
+    def __init__(self, df, treshv, filename, idx):
+        self.df = df
+        self.treshv = treshv
+        self.filename = filename
+        self.idx = idx
+
+    def filter_data(self):
+        var = self.df.var(axis=0)
+        drop_columns = []
+        for column, variance in enumerate(var):
+            if var[column] < self.treshv:
+                drop_columns.append(self.df.columns[column])
+        return self.df.drop(drop_columns, axis=1)
+
+    def plot_columns(self):
+        if self.idx == None:
+            for col in range(1, len(self.df.columns)):
+                self.df.plot("time", self.df.columns[col])
+                # the replace stuff I added just, because linux isn't happy with </> in the filename name
+                # plt.savefig(str(filename) + str(df.columns[col]).replace("<", "").replace(">", ""))
+                plt.show()
+                plt.close()
+        else:
+            self.df[self.df.columns[self.idx]].plot()
+            plt.savefig(str(self.filename) + str(self.df.columns[self.idx]).replace("<", "").replace(">", ""))
 
 class numerical_methods:
     pass
